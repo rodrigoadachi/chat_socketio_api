d35796129bef9a468020a038a2a2efbb187c6960
Rodrigo Adachi <rodrigo.adachi@digi.ag>
2023-09-08 20:22:52 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('ok')
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.APP_PORT || 3000;
+
+app.listen(port, () => console.log(`chat socketio running on port ${port}`));
